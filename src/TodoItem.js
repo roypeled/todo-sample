e4f0d0df40a97c5208f1ddeb456d353c8994f5a1
Roy Peled <royp@folloze.com>
2016-10-28 11:16:55 +0300

    progress

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,6 +6,32 @@ TodoItem.prototype.setCompleted = function(isCompleted){
 	this.isCompleted = isCompleted;
 };
 
+function TodoItemView(todoItem){
+	var template,
+		self = this;
+
+	function init(){
+		var source = document.getElementById("todoItem").innerHTML;
+		template = Handlebars.compile(source);
+		self.html = document.createElement("div");
+	}
+
+	function render(){
+		self.html.innerHTML = template(todoItem);
+		
+		self.html.querySelector(".toggle-completed")
+			.addEventListener("click", onSetCompletedClick);
+	}
+
+	function onSetCompletedClick(){
+		todoItem.setCompleted( !todoItem.isCompleted );
+		render();
+	}
+
+	init();
+	render();
+}
+
 
 var label = "This is a test label";
 var todoItem = new TodoItem(label);
